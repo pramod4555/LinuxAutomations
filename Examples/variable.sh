46c9d3fc6a795c11556d05e6834dafb56112b9d3
Administrator <pramod.gudepu@gmail.com>
2022-08-15 23:58:26 -0700

    varialbe added

---
+++ Examples/variable.sh
@@ -0,0 +1,7 @@
+date=2021-12-08
+echo hi todays date is ${date}
+
+## if we  want to get the data dynamically via variables we need to store that data and we need to refer it in the script
+#command substitution  var =$(command)
+#arthimatic substitution var=$((expression))
+
